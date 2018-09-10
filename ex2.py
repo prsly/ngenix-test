b65ab8ef448582b026ed459bc36fe18076f2db1b
Michael <i@prsly.ru>
2018-09-10 18:57:27 +0300

    pep8 req

--- ex2.py
+++ ex2.py
@@ -3,6 +3,12 @@ import sys
 from collections import Counter
 
 
+search = re.compile(r'^(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.)' +
+                    r'{3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)')
+with open('{i}'.format(i=sys.argv[1]), 'r') as file:
+    logs = [row.strip() for row in file]
+
+
 def show_usage():
     print("Нужен аргумент\nНапример: {i} 'name_of.log'".format(i=sys.argv[0]))
     sys.exit(1)
@@ -12,7 +18,7 @@ def top5():
     listResult = []
     for i in logs:
         listResult.append(''.join(search.findall(i)))
-    top5 = CounterDict(listResult, 5)
+    top5 = counter_dict(listResult, 5)
     for i in top5:
         print(i)
 
@@ -22,28 +28,24 @@ def error404():
     for i in logs:
         if search.findall(i) and i.find('404') != -1:
             listResult.append(''.join(search.findall(i)))
-    print(CounterDict(listResult))
+    print(counter_dict(listResult))
 
 
-def CounterDict(list_, count=1):
+def counter_dict(list_, count=1):
     return Counter(dict([(i, list_.count(i))
                    for i in set(list_)])).most_common(count)
 
 
-def activetime():
+def active_time():
     searchtime = re.compile(r'\d{2}:\d{2}:\d{2} ')
     listResult = []
     for i in logs:
         listResult.append(''.join(searchtime.findall(i))[0:2])
-    print(CounterDict(listResult))
+    print(counter_dict(listResult))
 
 
-search = re.compile(r'^(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.)' +
-                    r'{3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)')
 if len(sys.argv) != 2:
     show_usage()
-with open('{i}'.format(i=sys.argv[1]), 'r') as file:
-    logs = [row.strip() for row in file]
 choose = input('Введите пункт меню\n' +
                '1 - TOP-5 (по количеству запросов) IP-адресов\n' +
                '2 - IP-адрес, который больше всех получил 404 статус код' +
@@ -53,7 +55,7 @@ if choose == '1':
 elif choose == '2':
     error404()
 elif choose == '3':
-    activetime()
+    active_time()
 else:
     print('Неверный ввод')
     sys.exit(1)
